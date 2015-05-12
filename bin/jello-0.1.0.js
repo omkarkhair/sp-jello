3c15cea861f61db05eebd38d533294b4c64d1755
Omkar Khair <omkarkhair@gmail.com>
2015-05-12 18:20:52 +0530

    Created release 0.1

---
+++ bin/jello-0.1.0.js
@@ -0,0 +1,158 @@
+var Jello = function (siteUrl, listOptions) {
+
+    // Any initialization logic should go here
+    if (!listOptions.name || !listOptions.contentType || !siteUrl)
+    {
+        throw("Invalid settings");
+    }
+    
+    
+    return {
+        listName: listOptions.name,
+        listContentType: listOptions.contentType,
+        requestDigest: null,
+        getRequestDigest: function () {
+            var self = this;
+            var dfd = $.Deferred();
+            if (self.requestDigest && self.requestDigest.expiresOn > (new Date())) {
+                return dfd.resolve();
+            }
+            else {
+                $.ajax({
+                    type: "POST",
+                    url: "https://jello.sharepoint.com/sites/sprest/_api/contextinfo",
+                    headers: {
+                    "accept": "application/json;odata=verbose"
+                    }
+                }).done(function(resp){
+                    var now = (new Date()).getTime();
+                    self.requestDigest = resp.d.GetContextWebInformation;
+                    self.requestDigest.expiresOn = now + (resp.d.GetContextWebInformation.FormDigestTimeoutSeconds * 1000) - 60000; // -60000 To prevent any calls to fail at all, by refreshing a minute before
+                    console.log("Token", self.requestDigest.FormDigestValue);
+                    dfd.resolve();
+                })
+                .fail(function(err){
+                    console.log("Error fetching Request Digest. Some parts won't work.");
+                    dfd.reject();
+                });
+            }
+            
+            return dfd.promise();
+        },
+        get: function (id) {
+            var dfd = $.Deferred();
+            
+            var filter = ( id ) ? "(" + id + ")" : "" ;
+            
+            $.ajax({
+                type: 'GET',
+                headers: {
+                    "accept": "application/json;odata=verbose"
+                },
+                url: siteUrl + "/_api/web/lists/getbytitle('" + listOptions.name + "')/items" + filter
+            }).done(function (resp) {
+                dfd.resolve(resp);
+            }).fail(function (err) {
+                dfd.reject(err);
+            });
+
+            return dfd.promise();
+        },
+        add: function (item) {
+            var self = this;
+            var dfd = $.Deferred();
+            
+            this.getRequestDigest().then(function(){
+                item.__metadata = {
+                    type: self.listContentType
+                };
+                
+                var payload = JSON.stringify(item);
+                $.ajax({
+                    type: 'POST',
+                    headers: {
+                        "accept": "application/json;odata=verbose",
+                        "content-type": "application/json;odata=verbose",
+                        "X-RequestDigest": self.requestDigest.FormDigestValue
+                    },
+                    data: payload,
+                    url: siteUrl + "/_api/web/lists/getbytitle('" + listOptions.name + "')/items"
+                }).done(function (resp) {
+                    dfd.resolve(resp);
+                }).fail(function (err) {
+                    dfd.reject(err);
+                });
+            }, function (err) {
+                dfd.reject(err);
+            });
+            
+            return dfd.promise();
+        },
+        remove: function (id, etag) {
+            // if etag not provided, overwrite item even if outdated
+            if (!etag)
+                etag = "*";
+            
+            var self = this;
+            var dfd = $.Deferred();
+            
+            this.getRequestDigest().then(function(){
+                
+                $.ajax({
+                    type: 'POST',
+                    headers: {
+                        "X-RequestDigest": self.requestDigest.FormDigestValue,
+                        "X-HTTP-Method": "DELETE",
+                        "If-Match": etag
+                    },
+                    url: siteUrl + "/_api/web/lists/getbytitle('" + listOptions.name + "')/items(" + id + ")"
+                }).done(function (resp) {
+                    dfd.resolve(resp);
+                }).fail(function (err) {
+                    dfd.reject(err);
+                });
+            }, function (err) {
+                dfd.reject(err);
+            });
+            
+            return dfd.promise();
+        },
+        update: function (id, update, etag) {
+            
+            // if etag not provided, overwrite item even if outdated
+            if (!etag)
+                etag = "*";
+            
+            var self = this;
+            var dfd = $.Deferred();
+            
+            this.getRequestDigest().then(function(){
+                update.__metadata = {
+                    type: self.listContentType
+                };
+                
+                var payload = JSON.stringify(update);
+                $.ajax({
+                    type: 'POST',
+                    headers: {
+                        "accept": "application/json;odata=verbose",
+                        "content-type": "application/json;odata=verbose",
+                        "X-RequestDigest": self.requestDigest.FormDigestValue,
+                        "X-HTTP-Method": "MERGE",
+                        "If-Match": etag
+                    },
+                    data: payload,
+                    url: siteUrl + "/_api/web/lists/getbytitle('" + listOptions.name + "')/items(" + id + ")"
+                }).done(function (resp) {
+                    dfd.resolve(resp);
+                }).fail(function (err) {
+                    dfd.reject(err);
+                });
+            }, function (err) {
+                dfd.reject(err);
+            });
+            
+            return dfd.promise();
+        }
+    };
+}
